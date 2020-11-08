2c829e2b1a4294e7496f5464e196d29fc6ac4e30
ABDULLA SHINAH <103219744@student.swin.edu.au>
2020-11-09 09:29:42 +1100

    task 3 complete

--- Demo.sql
+++ Demo.sql
@@ -76,3 +76,47 @@ CREATE TABLE Booking
     FOREIGN KEY (TourName, EventYear, EventMonth, EventDay) References Event,
     FOREIGN KEY (ClientID) References Client,
 );
+
+INSERT INTO TOUR
+    (TourName, Description)
+VALUES
+    ('North', 'Tour of wineries and outlets of the Bedigo and Castlemaine region'),
+    ('South', 'Tour of wineries and outlets of Mornington Penisula'),
+    ('West', 'Tour of wineries and outlets of the Geelong and Otways region');
+
+INSERT INTO CLIENT 
+    (ClientID, Surname, GivenName, Gender)
+VALUES
+    (1, 'Price', 'Taylor', 'M'),
+    (2, 'Gamble', 'Ellyse', 'F'),
+    (3, 'Tan', 'Tilly', 'F'),
+    (103219744, 'Abdulla', 'Shinah', 'M');
+
+
+INSERT INTO EVENT
+    (TourName, EventYear, EventMonth, EventDay, EventFee)
+VALUES
+    ('North', 2016, 'Jan', 9, 200),
+    ('North', 2016, 'Feb', 13, 225),
+    ('South', 2016, 'Jan', 9, 200),
+    ('South', 2016, 'Jan', 16, 200),
+    ('West', 2016,'Jan', 29, 225);
+
+   
+INSERT INTO BOOKING 
+    (ClientID, TourName, EventYear, EventMonth, EventDay, DateBooked, Payment)
+VALUES
+    ( 1, 'North', 2016, 'Jan', 9, '2015-12-10', 200),
+    ( 2, 'North', 2016, 'Jan', 9, '2015-12-16', 200),
+    ( 1, 'North', 2016, 'Feb', 13, '2016-01-08', 225),
+    ( 2, 'North', 2016, 'Feb', 13, '2016-01-14', 125),
+    ( 3, 'North', 2016, 'Feb', 13, '2016-02-03', 225),
+    ( 1, 'South', 2016, 'Jan', 9, '2015-12-10', 200), 
+    ( 2, 'South', 2016, 'Jan', 16, '2015-12-18', 200),
+    ( 3, 'South', 2016, 'Jan', 16, '2016-01-09', 200),
+    ( 2, 'West',  2016, 'Jan', 29, '2015-12-17', 225),
+    ( 3, 'West',  2016, 'Jan', 29, '2015-12-18', 200),
+    ( 103219744, 'South', 2016, 'Jan', 16, '2015-12-16', 200),
+    ( 103219744, 'West', 2016, 'Jan', 29, '2015-12-16', 255),
+    ( 103219744, 'North', 2016, 'Jan', 9, '2015-12-16', 200);
+
