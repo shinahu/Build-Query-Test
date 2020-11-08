cef50732a4eb6017423bbadc06ba492029c5d3c4
ABDULLA SHINAH <103219744@student.swin.edu.au>
2020-11-09 09:11:40 +1100

    Task 1 Complete

--- Demo.sql
+++ Demo.sql
@@ -1,2 +1,22 @@
 /* RELATIONAL SCHEMA
-Abdulla shinah 103219744*/
+Abdulla shinah 103219744
+
+Task
+
+
+
+Tour (TourName, Description)
+PRIMARY KEY (TourName)
+
+Client (ClientID, Surname, GivenName, Gender)
+PRIMARY KEY (ClientID)
+
+Event (TourName, EventYear, EventMonth, EventDay, Fee)
+PRIMARY KEY (TourName, EventYear, EventMonth, EventDay)
+FOREIGN KEY (TourName) references Tour
+
+Booking (ClientID, TourName, EventYear, EventMonth, EventDay DateBooked, Payment)
+PRIMARY KEY (ClientID, TourName, EventYear, EventMonth, EventDay)
+FOREIGN KEY (TourName, EventYear, EventMonth, EventDay) References Event
+FOREIGN KEY (ClientID) References Client
+ */
